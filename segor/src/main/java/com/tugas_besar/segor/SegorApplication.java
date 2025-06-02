89a6cb99c11d70fbaf683b961a36714af14c29e6
Ragis Rahmatulloh <77946797+icarusnp1@users.noreply.github.com>
2025-06-03 00:47:08 +0700

    0.1
    Crate project spring folder

---
+++ segor/src/main/java/com/tugas_besar/segor/SegorApplication.java
@@ -0,0 +1,13 @@
+package com.tugas_besar.segor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SegorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SegorApplication.class, args);
+	}
+
+}
---
+++ segor/src/test/java/com/tugas_besar/segor/SegorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tugas_besar.segor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SegorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
