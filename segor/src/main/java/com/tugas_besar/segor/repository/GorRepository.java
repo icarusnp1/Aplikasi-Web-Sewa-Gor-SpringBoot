c690e343df2a6059fe6d8d0be029421b5f51c1b8
Ragis Rahmatulloh <77946797+icarusnp1@users.noreply.github.com>
2025-06-09 17:40:13 +0700

    Tambah fitur pencarian, baru di POV user

--- segor/src/main/java/com/tugas_besar/segor/repository/GorRepository.java
+++ segor/src/main/java/com/tugas_besar/segor/repository/GorRepository.java
@@ -3,8 +3,9 @@ package com.tugas_besar.segor.repository;
 import com.tugas_besar.segor.entity.GorEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 @Repository
 public interface GorRepository extends JpaRepository<GorEntity, Integer> {
-    
+    List<GorEntity> findByNamaContainingIgnoreCase(String nama);
 }
